33000f41b01d21be9f8b1b9547c4c2b434a76b65
Ruchin Munjal <emailruchin@gmail.com>
2023-07-09 20:29:11 +0100

    simple repo pattern

---
+++ repositories/product-file.js
@@ -0,0 +1,24 @@
+let repo = {};
+
+repo.get = function () {
+  return [
+    {
+      productId: 89,
+      name: "All purpose bike stand",
+    },
+    {
+      productId: 90,
+      name: "Bike wash disolver",
+    },
+    {
+      productId: 91,
+      name: "Cable Lock",
+    },
+    {
+      productId: 92,
+      name: "Chain",
+    },
+  ];
+};
+
+module.exports = repo;
