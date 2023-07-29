0a499bd2cf92f974a1dd28dc74a647ae2cd83f92
Ruchin Munjal <emailruchin@gmail.com>
2023-07-30 00:40:15 +0100

    open api specification added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('stackify-node-apm');
 const express = require("express");
+const swaggerUi=require("swagger-ui-express");
+const openApiDocumentation = require("../sample-api/openapi.json");
+
 
 const app = express();
 const router = express.Router();
@@ -9,6 +12,7 @@ const port = 3000;
 router.use('/product', require('./routes/product'));
 
 app.use("/api", router);
+app.use("/docs",swaggerUi.serve, swaggerUi.setup(openApiDocumentation))
 //Create web server to listen on specific port
 let server = app.listen(port, function () {
   console.log(`API server is running on http://localhost:${port}`);
