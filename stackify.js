3179cf79151969e17d10aa665f11dd9b0c2f0cc3
Ruchin Munjal <emailruchin@gmail.com>
2023-07-29 23:22:49 +0100

    added stackify support

---
+++ stackify.js
@@ -0,0 +1,15 @@
+/**
+ * Stackify Node APM Configuration
+ */
+exports.config = {
+  /**
+   * Your application name.
+   */
+  application_name: 'sample-api',
+  /**
+   * Your environment name.
+   */
+  environment_name: 'Production',
+
+  prefix_enabled:true
+}
